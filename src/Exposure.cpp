458e7c3b0812760cd2d497182c0276606e8731ea
Norbert Nopper <nopper@ux3d.io>
2022-03-26 23:04:04 +0100

    Final exposure.

--- src/Exposure.cpp
+++ src/Exposure.cpp
@@ -9,8 +9,22 @@ double calculateEV100(double N, double t, double S)
 	return glm::log2((N2/t) * (100.0/S));
 }
 
+double Yavg_2_EV100(double Yavg)
+{
+	return glm::log2(Yavg * 8.0);
+}
+
+double EV100_2_Yavg(double ev100)
+{
+	return glm::pow(2.0, ev100 - 3.0);
+}
+
+double EV100_2_Ymax(double ev100)
+{
+	return 1.2 * glm::pow(2.0, ev100);
+}
+
 double EV100_2_EXPOSURE(double ev100)
 {
-	double Lmax = 1.2 * glm::pow(2.0, ev100);
-	return 1.0 / Lmax;
+	return 1.0 / EV100_2_Ymax(ev100);
 }
--- src/Exposure.h
+++ src/Exposure.h
@@ -3,6 +3,7 @@
 
 // see https://interplayoflight.wordpress.com/2020/04/19/validating-physical-light-units/
 // see https://google.github.io/filament/Filament.md.html#imagingpipeline/physicallybasedcamera
+// see https://seblagarde.files.wordpress.com/2015/07/course_notes_moving_frostbite_to_pbr_v32.pdf
 // see https://github.com/troughton/PBRenderer/blob/master/Resources/Shaders/OpenGL/Camera.glsl
 
 // Exposure value see https://en.wikipedia.org/wiki/Exposure_value
@@ -21,6 +22,17 @@
 // <=> 	EV_100 					= log2((N^2/t) * (100/S))
 double calculateEV100(double N, double t, double S);
 
+// N^2/t = L*S/K
+//			K is the reflected-light meter calibration constant see https://en.wikipedia.org/wiki/Light_meter#Calibration_constants
+//
+// Solve
+//		N^2/t 		= L*S/K
+// <=>	log2(N^2/t)	= log2(L*S/K)			// with S=100 and K=12.5
+// <=>	EV_100		= log2(L*100/12.5)
+double Yavg_2_EV100(double Yavg);
+
+double EV100_2_Yavg(double ev100);
+
 // Luminous exposure see https://en.wikipedia.org/wiki/Film_speed
 //
 // H = q*L*t/N^2
@@ -34,6 +46,8 @@ double calculateEV100(double N, double t, double S);
 //		L			= 78/(S*q)*N^2/t
 //		L			= 78/(S*q)*2^EV_100			// with S=100 and q=0.65
 //		L			= 78/(100*0.65)*2^EV_100
+double EV100_2_Ymax(double ev100);
+
 double EV100_2_EXPOSURE(double ev100);
 
 #endif /* EXPOSURE_H_ */
